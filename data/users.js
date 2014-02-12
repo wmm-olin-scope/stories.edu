4522df051f6e2a19f50587b95c3b5c114965a096
Chase Kernan <chase.kernan@gmail.com>
2014-02-12 09:46:44 -0500

    Switch to bcryptjs
    Avoids build problems

--- data/users.js
+++ data/users.js
@@ -1,7 +1,7 @@
 
 var mongoose = require('mongoose')
   , _ = require('underscore');
-  //, bcrypt = require('bcrypt');
+  , bcrypt = require('bcryptjs');
 
 exports.userCategories = ["General", "Student", "Parent", "Educator", 
                           "Administrator", "WebAdmin"];
