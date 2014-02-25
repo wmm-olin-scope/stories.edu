084014167e647b3575552b7861839343c1e79fac
Madison May <madison.may@students.olin.edu>
2014-02-24 20:47:45 -0500

    Postcard modal

--- public/javascripts/postcard.coffee
+++ public/javascripts/postcard.coffee
@@ -1,21 +1,116 @@
 setup = ->
 
+    g = {}
+
     String::capitalize = ->
         return (word[0].toUpperCase() + word[1..-1].toLowerCase() for word in this.split /\s+/).join ' ';
 
-    schoolsearch = new Bloodhound(
-        datumTokenizer: (d) ->
-            Bloodhound.tokenizers.whitespace d.num
-        queryTokenizer: Bloodhound.tokenizers.whitespace
-        remote:
-            url: '/schools/by-name?text=%QUERY' 
-            filter: (schools) -> 
-                $.map schools, (school) ->
-                    school.name = school.name.capitalize()
-                    return school 
-    )
+    getStateSelect = -> $ '#state'
+    getCityInput = -> $ '#city'
+    getSchoolInput = -> $ '#school'
 
-    schoolsearch.initialize()
+    populateStateOption = ->
+        select = getStateSelect()
+        select.empty()
+        select.append "<option value=\"\">State</option>"
+        for state in stateList
+            select.append "<option value=\"#{state}\">#{state}</option>"
+        return
+
+     stateList = [
+        'AL','AK','AZ','AR','CA','CO','CT','DE','DC','FL','GA','HI','ID','IL',
+        'IN','IA','KS','KY','LA','ME','MD','MA','MI','MN','MS','MO','MT','NE',
+        'NV','NH','NJ','NM','NY','NC','ND','OH','OK','OR','PA','RI','SC','SD',
+        'TN','TX','UT','VT','VA','WA','WV','WI','WY','AS','GU','MP','PR','VI']
+
+    enableInput = (input, placeholder) -> 
+        input.attr('disabled', no).val('').attr('placeholder', placeholder)
+    disableInput = (input, placeholder) ->
+        input.typeahead 'destroy'
+        input.attr('disabled', yes).val('').attr('placeholder', placeholder)
+
+    findTransitions =
+        state: ->
+            getStateSelect().val ''
+            disableInput getCityInput(), 'Select a state first...'
+            disableInput getSchoolInput(), 'Select a state first...'
+            doStateSelection()
+        city: (state) ->
+            enableInput getCityInput(), 'City'
+            disableInput getSchoolInput(), 'Select a city first...'
+            doCitySelection state
+        school: (state, city) ->
+            enableInput getSchoolInput(), 'School'
+            doSchoolSelection state, city
+
+    doStateSelection = ->
+        oldState = null
+        getStateSelect().off 'change'
+        getStateSelect().change ->
+            state = getStateSelect().val()
+            if state is ''
+                findTransitions.state()
+            else if state isnt oldState
+                findTransitions.city state
+            oldState = state
+
+    makeHound = (options) ->
+        {url, filter, accessor} = options
+        hound = new Bloodhound
+            datumTokenizer: (d) -> 
+                Bloodhound.tokenizers.whitespace accessor d
+            queryTokenizer: Bloodhound.tokenizers.whitespace
+            prefetch:
+                url: url
+                filter: filter
+                ttl: 0 # TODO: when in production set to longer
+        hound.initialize()
+        hound
+
+    getCityHound = (state) -> makeHound
+        url: "/schools/cities/#{state}"
+        filter: (cities) -> 
+            {name: city, display: city.capitalize()} for city in cities
+        accessor: (city) -> city.name
+
+    doCitySelection = (state) ->
+        hound = getCityHound state
+        input = getCityInput()
+
+        input.typeahead 'destroy'
+        input.typeahead null,
+            name: 'cities'
+            displayKey: 'display'
+            source: hound.ttAdapter()
+        input.focus()
+
+        input.off 'typeahead:selected'
+        input.on 'typeahead:selected', (obj, city) -> 
+            findTransitions.school state, city
+
+    getSchoolHound = (state, city) -> makeHound
+        url: "/schools/by-city/#{state}/#{encodeURIComponent city.name}"
+        filter: (schools) ->
+            for school in schools
+                school.display = school.name.capitalize()
+            schools
+        accessor: (school) -> school.name
+
+    doSchoolSelection = (state, city) ->
+        hound = getSchoolHound state, city
+        input = getSchoolInput()
+
+        input.typeahead 'destroy'
+        input.typeahead null,
+            name: 'schools'
+            displayKey: 'display'
+            source: hound.ttAdapter()
+        input.focus()
+
+        input.off 'typeahead:selected'
+        input.on 'typeahead:selected', (obj, school) -> 
+            g.school = school
+            console.log g.school
 
     $('#teacher_name').keyup ->
         $('#mailto_name').val $(this).val()
@@ -29,20 +124,28 @@ setup = ->
         $('#school_modal').modal('show')
         return
 
+    $('#modal_submit').click ->
+        $('#school_modal').modal('hide')
+        console.log g.school
+        if g.school != undefined
+            $('#mailto_school').val g.school.name.capitalize()
+            $('#mailto_street').val g.school.mailingAddress.capitalize()
+            $('#mailto_city_state').val g.school.city.capitalize() + ", " + g.school.state + " " + g.school.zip
+
     $('#send_button').click ->
-        teacher_name = $('#teacher_name').text()
-        teacher_role = $('#teacher_role').text()
-        message = $('#freetext').text()
-        author_name = $('#author_name').text()
-        author_role = $('#author_role').text()
+        teacher_name = $('#teacher_name').val()
+        teacher_role = $('#teacher_role').val()
+        message = $('#freetext').val()
+        author_name = $('#author_name').val()
+        author_role = $('#author_role').val()
         anon_request = $('#checkbox_input').is(':checked')
-        return_name = $('#return_name').text()
-        return_email = $('#return_email').text()
-        mailto_name = $('#mailto_name').text()
-        mailto_role = $('#mailto_role').text()
-        mailto_school = $('#mailto_school').text()
-        mailto_street = $('#mailto_street').text()
-        mailto_city_state = $('#mailto_city_state').text()
+        return_name = $('#return_name').val()
+        return_email = $('#return_email').val()
+        mailto_name = $('#mailto_name').val()
+        mailto_role = $('#mailto_role').val()
+        mailto_school = $('#mailto_school').val()
+        mailto_street = $('#mailto_street').val()
+        mailto_city_state = $('#mailto_city_state').val()
         contents = {"teacher_name": teacher_name, "teacher_role": teacher_role, "message": message, 
         "author_name": author_name, "author_role": author_role, "anon_request": anon_request, 
         "return_name": return_name, "return_email": return_email, "mailto_name": mailto_name, 
@@ -51,15 +154,9 @@ setup = ->
         console.log(contents)
         return contents
 
-    $("#mailto_school").typeahead null,
-        displayKey: "name"
-        source: schoolsearch.ttAdapter()
-        minLength: 4
+    populateStateOption()
+    findTransitions.state()
 
-    $("#mailto_school").bind "typeahead:selected", (event, data, dataset) ->
-        $('#mailto_street').val(data.mailingAddress.capitalize())
-        $('#mailto_city_state').val(data.city.capitalize() + ", " + data.state + " " + data.zip)
-        return
     return
 $ ->
     setup()
--- public/javascripts/postcard.js
+++ public/javascripts/postcard.js
@@ -2,7 +2,8 @@
 var setup;
 
 setup = function() {
-  var schoolsearch;
+  var disableInput, doCitySelection, doSchoolSelection, doStateSelection, enableInput, findTransitions, g, getCityHound, getCityInput, getSchoolHound, getSchoolInput, getStateSelect, makeHound, populateStateOption, stateList;
+  g = {};
   String.prototype.capitalize = function() {
     var word;
     return ((function() {
@@ -16,22 +17,151 @@ setup = function() {
       return _results;
     }).call(this)).join(' ');
   };
-  schoolsearch = new Bloodhound({
-    datumTokenizer: function(d) {
-      return Bloodhound.tokenizers.whitespace(d.num);
-    },
-    queryTokenizer: Bloodhound.tokenizers.whitespace,
-    remote: {
-      url: '/schools/by-name?text=%QUERY',
-      filter: function(schools) {
-        return $.map(schools, function(school) {
-          school.name = school.name.capitalize();
-          return school;
-        });
-      }
+  getStateSelect = function() {
+    return $('#state');
+  };
+  getCityInput = function() {
+    return $('#city');
+  };
+  getSchoolInput = function() {
+    return $('#school');
+  };
+  populateStateOption = function() {
+    var select, state, _i, _len;
+    select = getStateSelect();
+    select.empty();
+    select.append("<option value=\"\">State</option>");
+    for (_i = 0, _len = stateList.length; _i < _len; _i++) {
+      state = stateList[_i];
+      select.append("<option value=\"" + state + "\">" + state + "</option>");
     }
-  });
-  schoolsearch.initialize();
+  };
+  stateList = ['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY', 'AS', 'GU', 'MP', 'PR', 'VI'];
+  enableInput = function(input, placeholder) {
+    return input.attr('disabled', false).val('').attr('placeholder', placeholder);
+  };
+  disableInput = function(input, placeholder) {
+    input.typeahead('destroy');
+    return input.attr('disabled', true).val('').attr('placeholder', placeholder);
+  };
+  findTransitions = {
+    state: function() {
+      getStateSelect().val('');
+      disableInput(getCityInput(), 'Select a state first...');
+      disableInput(getSchoolInput(), 'Select a state first...');
+      return doStateSelection();
+    },
+    city: function(state) {
+      enableInput(getCityInput(), 'City');
+      disableInput(getSchoolInput(), 'Select a city first...');
+      return doCitySelection(state);
+    },
+    school: function(state, city) {
+      enableInput(getSchoolInput(), 'School');
+      return doSchoolSelection(state, city);
+    }
+  };
+  doStateSelection = function() {
+    var oldState;
+    oldState = null;
+    getStateSelect().off('change');
+    return getStateSelect().change(function() {
+      var state;
+      state = getStateSelect().val();
+      if (state === '') {
+        findTransitions.state();
+      } else if (state !== oldState) {
+        findTransitions.city(state);
+      }
+      return oldState = state;
+    });
+  };
+  makeHound = function(options) {
+    var accessor, filter, hound, url;
+    url = options.url, filter = options.filter, accessor = options.accessor;
+    hound = new Bloodhound({
+      datumTokenizer: function(d) {
+        return Bloodhound.tokenizers.whitespace(accessor(d));
+      },
+      queryTokenizer: Bloodhound.tokenizers.whitespace,
+      prefetch: {
+        url: url,
+        filter: filter,
+        ttl: 0
+      }
+    });
+    hound.initialize();
+    return hound;
+  };
+  getCityHound = function(state) {
+    return makeHound({
+      url: "/schools/cities/" + state,
+      filter: function(cities) {
+        var city, _i, _len, _results;
+        _results = [];
+        for (_i = 0, _len = cities.length; _i < _len; _i++) {
+          city = cities[_i];
+          _results.push({
+            name: city,
+            display: city.capitalize()
+          });
+        }
+        return _results;
+      },
+      accessor: function(city) {
+        return city.name;
+      }
+    });
+  };
+  doCitySelection = function(state) {
+    var hound, input;
+    hound = getCityHound(state);
+    input = getCityInput();
+    input.typeahead('destroy');
+    input.typeahead(null, {
+      name: 'cities',
+      displayKey: 'display',
+      source: hound.ttAdapter()
+    });
+    input.focus();
+    input.off('typeahead:selected');
+    return input.on('typeahead:selected', function(obj, city) {
+      return findTransitions.school(state, city);
+    });
+  };
+  getSchoolHound = function(state, city) {
+    return makeHound({
+      url: "/schools/by-city/" + state + "/" + (encodeURIComponent(city.name)),
+      filter: function(schools) {
+        var school, _i, _len;
+        for (_i = 0, _len = schools.length; _i < _len; _i++) {
+          school = schools[_i];
+          school.display = school.name.capitalize();
+        }
+        return schools;
+      },
+      accessor: function(school) {
+        return school.name;
+      }
+    });
+  };
+  doSchoolSelection = function(state, city) {
+    var hound, input;
+    hound = getSchoolHound(state, city);
+    input = getSchoolInput();
+    input.typeahead('destroy');
+    input.typeahead(null, {
+      name: 'schools',
+      displayKey: 'display',
+      source: hound.ttAdapter()
+    });
+    input.focus();
+    input.off('typeahead:selected');
+    return input.on('typeahead:selected', function(obj, school) {
+      g.school = school;
+      return console.log(g.school);
+    });
+  };
   $('#teacher_name').keyup(function() {
     $('#mailto_name').val($(this).val());
   });
@@ -41,21 +171,30 @@ setup = function() {
   $('#mailto_school').focus(function() {
     $('#school_modal').modal('show');
   });
+  $('#modal_submit').click(function() {
+    $('#school_modal').modal('hide');
+    console.log(g.school);
+    if (g.school !== void 0) {
+      $('#mailto_school').val(g.school.name.capitalize());
+      $('#mailto_street').val(g.school.mailingAddress.capitalize());
+      return $('#mailto_city_state').val(g.school.city.capitalize() + ", " + g.school.state + " " + g.school.zip);
+    }
+  });
   $('#send_button').click(function() {
     var anon_request, author_name, author_role, contents, mailto_city_state, mailto_name, mailto_role, mailto_school, mailto_street, message, return_email, return_name, teacher_name, teacher_role;
-    teacher_name = $('#teacher_name').text();
-    teacher_role = $('#teacher_role').text();
-    message = $('#freetext').text();
-    author_name = $('#author_name').text();
-    author_role = $('#author_role').text();
+    teacher_name = $('#teacher_name').val();
+    teacher_role = $('#teacher_role').val();
+    message = $('#freetext').val();
+    author_name = $('#author_name').val();
+    author_role = $('#author_role').val();
     anon_request = $('#checkbox_input').is(':checked');
-    return_name = $('#return_name').text();
-    return_email = $('#return_email').text();
-    mailto_name = $('#mailto_name').text();
-    mailto_role = $('#mailto_role').text();
-    mailto_school = $('#mailto_school').text();
-    mailto_street = $('#mailto_street').text();
-    mailto_city_state = $('#mailto_city_state').text();
+    return_name = $('#return_name').val();
+    return_email = $('#return_email').val();
+    mailto_name = $('#mailto_name').val();
+    mailto_role = $('#mailto_role').val();
+    mailto_school = $('#mailto_school').val();
+    mailto_street = $('#mailto_street').val();
+    mailto_city_state = $('#mailto_city_state').val();
     contents = {
       "teacher_name": teacher_name,
       "teacher_role": teacher_role,
@@ -74,15 +213,8 @@ setup = function() {
     console.log(contents);
     return contents;
   });
-  $("#mailto_school").typeahead(null, {
-    displayKey: "name",
-    source: schoolsearch.ttAdapter(),
-    minLength: 4
-  });
-  $("#mailto_school").bind("typeahead:selected", function(event, data, dataset) {
-    $('#mailto_street').val(data.mailingAddress.capitalize());
-    $('#mailto_city_state').val(data.city.capitalize() + ", " + data.state + " " + data.zip);
-  });
+  populateStateOption();
+  findTransitions.state();
 };
 
 $(function() {
